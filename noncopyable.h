6c6812b04dd9eabaa200b822933eb0ad5e3e9b2a
MKMQ99 <904423848@qq.com>
2023-03-28 03:51:36 -0700

    finish noncopyable class

---
+++ noncopyable.h
@@ -0,0 +1,12 @@
+#pragma once 
+
+// noncopyableable被继承以后，派生类对象可以正常的构造和析构，但是
+// 派生类对象无法进行拷贝构造和赋值操作
+class noncopyable{
+    public:
+        noncopyable(const noncopyable&) = delete;
+        void operator=(const noncopyable&) = delete;
+    protected:
+        noncopyable() = default;
+        ~noncopyable() = default;
+}
