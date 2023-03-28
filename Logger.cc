a78080b90986a436c904103e96771442ad546f60
MKMQ99 <904423848@qq.com>
2023-03-28 05:05:56 -0700

    add Logger

---
+++ Logger.cc
@@ -0,0 +1,38 @@
+#include "Logger.h"
+#include <iostream>
+
+// 获取日志唯一的实例对象
+Logger* Logger::instance(){
+    if log_ == nullptr{
+        log_ = new Logger();
+    }
+    return log_;
+}
+
+// 设置日志级别
+void Logger::setLogLevel(int Level){
+    LogLevel_ = level;
+}
+
+// 写日志 [级别信息] time : msg
+void Logger::log(std::string msg){
+    switch (LogLevel_){
+        case INFO :
+            std::cout << "[INFO]";
+            break;
+        case ERROR :
+            std::cout << "[ERROR]";
+            break;
+        case FATAL :
+            std::cout << "[FATAL]";
+            break;
+        case DEBUG :
+            std::cout << "[DEBUG]";
+            break;
+        default:
+            break;
+    }
+
+    // 打印时间和msg
+    std::cout << "print time" << " : " << msg << std::endl;
+}
---
+++ Logger.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "noncopyable.h"
+
+// LOG_INFO("%s %d, arg1, arg2")
+#define LOG_INFO(logMsgFormat, ...) \
+    do { \
+        Logger &logger = Logger::instance(); \
+        logger.setLogLevel(INFO); \
+        char buf[1024] = {0}; \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
+        logger.log(buf); \
+    } while(0)
+
+#define LOG_ERROR(logMsgFormat, ...) \
+    do { \
+        Logger &logger = Logger::instance(); \
+        logger.setLogLevel(ERROR); \
+        char buf[1024] = {0}; \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
+        logger.log(buf); \
+    } while(0)
+
+#define LOG_FATAL(logMsgFormat, ...) \
+    do { \
+        Logger &logger = Logger::instance(); \
+        logger.setLogLevel(FATAL); \
+        char buf[1024] = {0}; \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
+        logger.log(buf); \
+    } while(0)
+
+#ifdef MUDEBUG
+#define LOG_DEBUG(logMsgFormat, ...) \
+    do { \
+        Logger &logger = Logger::instance(); \
+        logger.setLogLevel(DEBUG); \
+        char buf[1024] = {0}; \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
+        logger.log(buf); \
+    } while(0) 
+#else
+    #define LOG_DEBUG(logMsgFormat, ...)
+#endif
+
+// 定义日志级别
+// INFO 正常日志输出
+// ERROR 不影响运行的错误 
+// FATAL 严重错误
+// DEBUG 调试信息
+
+enum LogLevel{
+    INFO,
+    ERROR,
+    FATAL,
+    DEBUG,
+};
+
+// 输出一个日志类
+class Logger : noncopyable{
+    public:
+        // 获取日志唯一的实例对象
+        static Logger* instance();
+        // 设置日志级别
+        void setLogLevel(int Level);
+        // 写日志
+        void log(std::string msg);
+    private:
+        static Logger* log_;
+        int LogLevel_;
+        Logger(){}
+}
