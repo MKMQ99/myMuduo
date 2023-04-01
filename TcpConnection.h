c89722eddf844fdc989943501308528a8d82d0d4
MKMQ99 <904423848@qq.com>
2023-03-31 21:16:55 -0700

    add TcpConnection func member

--- TcpConnection.h
+++ TcpConnection.h
@@ -6,6 +6,7 @@
 #include <InetAddress.h>
 #include "Callbacks.h"
 #include "Buffer.h"
+#include "Timestamp.h"
 
 class Channel;
 class EventLoop;
@@ -25,9 +26,40 @@ class TcpConnection : noncopyable, public std::enable_shared_from_this<TcpConnec
                 , const InetAddress& peerAddr);
         ~TcpConnection();
 
+        EventLoop* getLoop() const { return loop_; }
+        const std::string& name() const { return name_; }
+        const InetAddress& localAddress() const { return localAddr_; }
+        const InetAddress& peerAddress() const { return peerAddr_; }
         
+        bool connected() const { return state_ == kConnected; }
+        bool disconnected() const { return state_ == kDisconnected; }
+
+        // 发送数据
+        void send(const void* message, int len);
+        // 关闭连接
+        void shutdown();
+
+        void setConnectionCallback(const ConnectionCallback& cb){ connectionCallback_ = cb; }
+        void setMessageCallback(const MessageCallback& cb){ messageCallback_ = cb; }
+        void setWriteCompleteCallback(const WriteCompleteCallback& cb){ writeCompleteCallback_ = cb; }
+        void setHighWaterMarkCallback(const HighWaterMarkCallback& cb, size_t highWaterMark){ highWaterMarkCallback_ = cb; highWaterMark_ = highWaterMark; }
+
+        // 连接建立
+        void connectEstablished();
+        // 连接销毁
+        void connectDestroyed();
+
     private:
         enum StateE  { kDisconnected, kConnecting, kConnected, kDisconnecting };
+        
+        void handleRead(Timestamp receiveTime);
+        void handleWrite();
+        void handleClose();
+        void handleError();
+
+        void sendInLoop(const void* message, size_t len);
+        void shutdownInLoop();
+
         EventLoop *loop_; // 这里绝对不是baseLoop，因为TcpConnection都是在subLoop里管理的
         const std::string name_;
         std::atomic_int state_;
