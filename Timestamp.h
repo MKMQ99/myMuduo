a1c2dde9dc622c7741ff65251d9d0a5ea8b2d665
MKMQ99 <904423848@qq.com>
2023-04-09 23:39:36 -0700

    update Http module, finish HttpRequest class

--- Timestamp.h
+++ Timestamp.h
@@ -8,6 +8,7 @@ class Timestamp{
         explicit Timestamp(int64_t microSecondsSinceEpoch);
         static Timestamp now();
         std::string toString() const;
+        void swap(Timestamp& that) { std::swap(microSecondsSinceEpoch_, that.microSecondsSinceEpoch_); }
     private:
         int64_t microSecondsSinceEpoch_;
 };
